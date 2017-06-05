1fbb94a7744affed4401dfde23e2c4106887714c
Zach Pomerantz <zmp@umich.edu>
2017-06-05 13:42:05 +0000

    add kn checker

---
+++ kn.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+check() {
+    let k=$1
+    let n=$2
+    vector=""
+    for i in $(seq 0 $n); do
+        if [[ "$i" -lt "$k" ]]; then
+            vector+=0
+        else
+            vector+=1
+        fi
+    done
+    let soln=`./solver -s $vector | awk '{print $NF}'`
+    echo $k $n $vector $soln
+    if [[ $(($k * ($n - $k + 1))) -ne "$soln" ]]; then
+        echo Failed check, should be $(($k * ($n - $k + 1)))
+        exit 1
+    fi
+}
+
+for n in {2..10}; do
+    for k in $(seq 1 $n); do
+        check $k $n
+    done
+done
